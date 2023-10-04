32fa75b8b720eae3898379b1280d3e4778cbc3bd
Alena Lyalyushkina <pluto_16@mail.ru>
2023-10-04 13:49:36 +1000

    feat: добавить скролл на якорь

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -14,7 +14,7 @@ function Main() {
       <Header />
       <main className='main'>
         <Promo />
-        <AboutProject />
+        <AboutProject id='about-project'/>
         <Techs />
         <AboutMe />
         <Portfolio />
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,3 +1,7 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import { Link, animateScroll as scroll } from 'react-scroll';
+
 import './Promo.css';
 
 function Promo() {
@@ -5,12 +9,26 @@ function Promo() {
     <section className='promo page__centered'>
       <div className='promo__text'>
         <h1 className='promo__header'>
-          Учебный проект студента факультета<br />Веб&#8209;разработки.
+          Учебный проект студента факультета
+          <br />
+          Веб&#8209;разработки.
         </h1>
-        <p className="promo__description">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-        <button className="button promo__button">Узнать больше</button>
+        <p className='promo__description'>
+          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+        </p>
+        <Link
+          to='about-project'
+          activeClass='active'
+          spy={true}
+          smooth={true}
+          offset={30}
+          duration={500}
+          className='button promo__button'
+        >
+          Узнать больше
+        </Link>
       </div>
-      <div className="promo__image"></div>
+      <div className='promo__image'></div>
     </section>
   );
 }
